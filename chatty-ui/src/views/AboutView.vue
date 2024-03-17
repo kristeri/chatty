aed8e166c21cb80834d84866d1497a4cd2cb33f8
Krister <krister.torikka@gmail.com>
2024-03-17 15:12:38 +0200

    Add information to about view

--- chatty-ui/src/views/AboutView.vue
+++ chatty-ui/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <p>Chatty is a chatbot based on Meta's LLaMA large language model.</p>
+    <p>Note, queries may take up to minutes to answer depending on the GPU.</p>
   </div>
 </template>
 
